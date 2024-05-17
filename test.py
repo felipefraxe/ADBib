142250c47353c667b1f150e85fdc4ae4e7566776
Felipe Fraxe <felipe_fraxe@hotmail.com>
2024-05-17 17:04:44 -0400

    teste média zero

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 from ADBib import *
 
 def main():
-    data = [-0.04, -0.19, 0.14, -0.09, -0.14, 0.19, 0.04, 0.09]
-
-    print(arithmetic_mean(data))
+    confidence_degree = [0.9, 0.95, 0.99]
+    sys_a = [5.3, 16, 0.6, 1.4, 0.6, 7.7, 3.6, 2.4, 12, 6, 57, 2, 1, 4, 6, 4, 8, 1]
+    sys_b = [19, 3.5, 3.3, 2.5, 3.6, 1.7, 12, 2, 8, 1, 1, 4]
+    for confidence in confidence_degree:
+        print(f"Amostras são significativamente similares: {zero_mean_test(sys_a, sys_b, confidence)}")
 
 if __name__ == "__main__":
     main()
